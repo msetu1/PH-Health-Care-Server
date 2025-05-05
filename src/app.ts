fa144633aa6e47a86cf09b9817af3fecd787d958
msetu1 <msetu5763@gmail.com>
2025-05-05 23:55:15 +0600

    global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import router from "./app/routes";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 
 const app: Application = express();
 app.use(cors());
@@ -14,5 +15,5 @@ app.use("/api/user", router);
 app.get("/", (req: Request, res: Response) => {
   res.send("PH Health Care Server is running...");
 });
-
+app.use(globalErrorHandler);
 export default app;
